5ffb89a9f7cadcaace362fa9473c03df93b271c7
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-04-05 09:26:16 +0700

    Update handlers.py

--- Cutiepii_Robot/modules/helper_funcs/handlers.py
+++ Cutiepii_Robot/modules/helper_funcs/handlers.py
@@ -5,48 +5,11 @@ from Cutiepii_Robot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES)
 from telegram import MessageEntity, Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
 from time import sleep
-from pyrate_limiter import (BucketFullException, Duration, RequestRate, Limiter,
-                            MemoryListBucket)
 
 if ALLOW_EXCL:
     CMD_STARTERS = ('/', '!')
 else:
     CMD_STARTERS = ('/',)
-
-
-class AntiSpam:
-
-    def __init__(self):
-        self.whitelist = (DEV_USERS or []) + (DRAGONS or []) + (
-            WOLVES or []) + (DEMONS or []) + (
-                TIGERS or [])
-        #Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
-        Duration.CUSTOM = 15  # Custom duration, 15 seconds
-        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
-        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
-        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
-        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
-        self.limiter = Limiter(
-            self.sec_limit,
-            self.min_limit,
-            self.hour_limit,
-            self.daily_limit,
-            bucket_class=MemoryListBucket)
-
-    def check_user(self, user):
-        """
-        Return True if user is to be ignored else False
-        """
-        if user in self.whitelist:
-            return False
-        try:
-            self.limiter.try_acquire(user)
-            return False
-        except BucketFullException:
-            return True
-
-
-SpamChecker = AntiSpam()
 
 
 class CustomCommandHandler(CommandHandler):
